49d91f4a2159f37641d11fa7ee6ea2c82e2730dd
erichris2902 <78182292+erichris2902@users.noreply.github.com>
2025-10-13 10:27:24 -0600

    Add pillow dependency for docx import

--- ikigai2025/settings.py
+++ ikigai2025/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-for-devel
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'https://lletrabase-eab0df519ae0.herokuapp.com,localhost,127.0.0.1').split(',')
 
 # Application definition
 
