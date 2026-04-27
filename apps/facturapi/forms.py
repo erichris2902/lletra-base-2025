1432c290b1a6d9a3baa6f2bdb3396a9fe1138201
erichris2902 <78182292+erichris2902@users.noreply.github.com>
2026-04-27 14:00:16 -0600

    asd

--- apps/facturapi/forms.py
+++ apps/facturapi/forms.py
@@ -21,7 +21,7 @@ uuid_validator = RegexValidator(
 
 class FacturapiInvoicePaymentForm(BaseModelForm):
     payment_invoice = forms.ModelChoiceField(
-        queryset=FacturapiInvoice.objects.filter(payment_form="PPD").filter(payment_method="99").filter(type="I").order_by('-stamp_date')[:5000],
+        queryset=FacturapiInvoice.objects.filter(payment_form="PPD").filter(payment_method="99").filter(type="I").order_by('-stamp_date')[:1000],
         label="Factura a pagar",
         widget=forms.Select(attrs={"class": "form-select"})
     )
